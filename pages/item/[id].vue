2f1620f0a383a3cfb2868e3a61eff4aaec0c46b6
AhrarHussain <ahrarhussain@AhrarHussains-MacBook-Pro.local>
2023-12-07 19:50:00 +0530

    single-product page

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -24,9 +24,9 @@
                     </div>
                 </div>
                 <div class="md:w-[60%] bg-white p-3 rounded-lg">
-                    <div v-if="product && product.data">
-                        <p class="mb-2">{{ product.data.title }}</p>
-                        <p class="font-light text-[12px] mb-2">{{ product.data.description }}</p>
+                    <div v-if="true">
+                        <p class="mb-2">Title</p>
+                        <p class="font-light text-[12px] mb-2">Description Section</p>
                     </div>
 
                     <div class="flex items-center pt-1.5">
@@ -35,6 +35,52 @@
                         </span>
                         <p class="text-[#FF5353]">Extra 5% off</p>
                     </div>
+
+                    <div class="flex items-center justify-start my-2">
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <Icon name="ic:baseline-star" color="#FF5353"/>
+                        <span class="text-[13px] font-light ml-2">5 213 Reviews 1,000+ orders</span>
+                    </div>
+
+                    <div class="border-b" />
+
+                    <div class="flex items-center justify-start gap-2 my-2">
+                        <div class="text-xl font-bold">$ {{ priceComputed }}</div>
+                        <span class="bg-[#F5F5F5] border text-[#C08562] text-[9px] font-semibold px-1.5 rounded-sm">70% off</span>
+                    </div>
+
+                    <p class="text-[#009A66] text-xs font-semibold pt-1">
+                        Free 11-day delivery over ￡8.28
+                    </p>
+
+                    <p class="text-[#009A66] text-xs font-semibold pt-1">
+                        Free Shipping
+                    </p>
+
+                    <div class="py-2"/>
+
+                    <button 
+                        @click="addToCart()"
+                        :disabled="isInCart"
+                        class="
+                            px-6 
+                            py-2 
+                            rounded-lg 
+                            text-white 
+                            text-lg 
+                            font-semibold 
+                            bg-gradient-to-r 
+                            from-[#FF851A] 
+                            to-[#FFAC2C]
+                        "
+                    >
+                        <div v-if="isInCart">Is Added</div>
+                        <div v-else>Add to Cart</div>
+                    </button>
+
                 </div>
             </div>
         </div>
@@ -43,6 +89,9 @@
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+const route = useRoute()
 
 let currentImage = ref(null)
 
@@ -53,7 +102,19 @@ onMounted(() => {
     })
 })
 
+const isInCart = computed(() => {
+    let res = false
+    userStore.cart.forEach(prod => {
+        if (route.params.id == prod.id) {
+            res = true
+        }
+    })
+    return res
+})
 
+const priceComputed = computed(() => {
+    return '26.40'
+})
 
 const images = ref([
     '',
@@ -64,4 +125,7 @@ const images = ref([
     'https://picsum.photos/id/144/800/800',
 ])
 
+const addToCart = () => {
+    alert('Added')
+}
 </script>
