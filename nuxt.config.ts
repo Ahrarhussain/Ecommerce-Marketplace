828c4ca0ca2977b7f12b513ebd5f34c96263b2b4
AhrarHussain <ahrarhakeem@gmail.com>
2025-03-10 16:16:29 +0530

    packages updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    pages: true,
-    modules: [
-        'nuxt-icon',
-        'nuxt-lodash',
-        '@pinia/nuxt',
-        '@pinia-plugin-persistedstate/nuxt',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/supabase'
-    ],
-    runtimeConfig: {
-        public: {
-          stripePk: process.env.STRIPE_PK_KEY
-        }
-    },
-    app: {
-        head: {
-          script: [
-            { src: 'https://js.stripe.com/v3/', defer: true }
-          ],
-        }
+  pages: true,
+
+  modules: [
+      'nuxt-icon',
+      'nuxt-lodash',
+      '@pinia/nuxt',
+      '@pinia-plugin-persistedstate/nuxt',
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/supabase'
+  ],
+
+  supabase:{
+      url: process.env.SUPABASE_URL,
+      key: process.env.SUPABASE_KEY,
+  },
+
+  runtimeConfig: {
+      public: {
+        stripePk: process.env.STRIPE_PK_KEY
       }
+  },
+
+  app: {
+      head: {
+        script: [
+          { src: 'https://js.stripe.com/v3/', defer: true }
+        ],
+      }
+    },
+
+  compatibilityDate: '2025-03-10'
 })
